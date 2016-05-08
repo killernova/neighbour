477eb5c0a7afbe8786e4e2dcb9903c27502b4c7f
killernova <enternicky@126.com>
2016-05-08 12:32:28 +0800

    modify the tag display

--- app/models/community_service.rb
+++ app/models/community_service.rb
@@ -12,8 +12,16 @@ class CommunityService < ActiveRecord::Base
   scope :fwzs, -> { where(tag: 2) }
   scope :bmxx, -> { where(tag: 3) }
 
-
   def list_pic
     other_img (self.photos.try :first), 'mini'
+  end
+
+  def tag_name
+    case tag
+    when 'esxx' then '二手信息'
+    when 'jzfw' then '家政服务'
+    when 'esxx' then '房屋租售'
+    when 'esxx' then '便民信息'
+    end
   end
 end
